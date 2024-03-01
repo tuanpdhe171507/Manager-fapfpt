28ba153b01b609ca06b49922e532af85538283fa
tuanpdhe171507 <161819748+tuanpdhe171507@users.noreply.github.com>
2024-03-02 02:43:26 +0700

    Update StudentDBContext.java
    chỉnh lại câu lệnh lấy dữ liệu ra từ bảng Attendance trong SQL và viết main test code

--- MyProjectPRJ301/src/java/dao/attendence/StudentDBContext.java
+++ MyProjectPRJ301/src/java/dao/attendence/StudentDBContext.java
@@ -6,12 +6,14 @@ package dao.attendence;
 
 import dao.*;
 import dao.timetable.GroupDBContext;
+import dao.timetable.SessionDBContext;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.Attendence;
 import model.Group;
 import model.Semeter;
 import model.Student;
@@ -50,17 +52,58 @@ public class StudentDBContext extends DBContext {
         }
         return null;
     }
+     public void takeAttendances(int seid, ArrayList<Attendence> atts) {
+        try {
+            connection.setAutoCommit(false);
+            String sql_remove_atts = "delete from Attendence where SessionID=?";
+            PreparedStatement stm_remove_atts = connection.prepareStatement(sql_remove_atts);
+            stm_remove_atts.setInt(1, seid);
+            stm_remove_atts.executeUpdate();
+
+            for (Attendence att : atts) {
+                String sql_insert_att = "INSERT INTO [Attendence](isPresent,Comment,SessionID,StudentID,RecordTime)\n"
+                        + "values(?,?,?,?,GETDATE())";
+                PreparedStatement stm_insert_att = connection.prepareStatement(sql_insert_att);
+                stm_insert_att.setBoolean(1,att.getIsPresent());
+                stm_insert_att.setString(2, att.getComment());
+                stm_insert_att.setInt(3, seid);
+                stm_insert_att.setInt(4, att.getStudent().getId());
+                stm_insert_att.executeUpdate();
+            }
+
+            String sql_update_lession = "UPDATE Session SET IsTaken =1 where SessionID=?";
+            PreparedStatement stm_update_lession = connection.prepareStatement(sql_update_lession);
+            stm_update_lession.setInt(1, seid);
+            stm_update_lession.executeUpdate();
+
+            connection.commit();
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            try {
+                connection.rollback();
+            } catch (SQLException ex1) {
+                Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException ex) {
+                Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+
 
     public ArrayList<Student> getLitsStudentByID(int id) {
         ArrayList<Student> lits = new ArrayList<>();
-        String sql = "Select gr.GroupID from Student as s \n"
+        String sql = "Select s.StudentID,s.Code,s.RollNumber,s.LastName,s.FirstName,s.Address,s.Gender,s.GroupID,s.Image from Student as s \n"
                 + "INNER join GroupStudent as g\n"
                 + "On s.StudentID=g.StudentID \n"
                 + "Inner join [Session] as se\n"
                 + "On se.GroupID= g.GroupID\n"
                 + "Inner join [Group] as gr\n"
                 + "On gr.GroupID=se.GroupID\n"
-                + "\n"
                 + "where se.SessionID=?";
         try {
             PreparedStatement st = connection.prepareStatement(sql);
@@ -68,7 +111,7 @@ public class StudentDBContext extends DBContext {
             ResultSet rs = st.executeQuery();
             while (rs.next()) {
                 Student student = new Student();
-                
+                student.setId(rs.getInt("StudentID"));
                 student.setRollNumber(rs.getString("RollNumber"));
                 student.setCode(rs.getString("Code"));
                 student.setFristName(rs.getString("FirstName"));
@@ -86,7 +129,7 @@ public class StudentDBContext extends DBContext {
         return lits;
     }
 
-      public static void main(String[] args) {
+    public static void main(String[] args) {
         // Tạo một đối tượng StudentDBContext
         StudentDBContext studentDBContext = new StudentDBContext();
 
@@ -104,10 +147,11 @@ public class StudentDBContext extends DBContext {
             System.out.println("First Name: " + student.getFristName());
             System.out.println("Last Name: " + student.getLastName());
             System.out.println("Image: " + student.getImage());
-           
+
             System.out.println("Address: " + student.getAddress());
             System.out.println("Group ID: " + student.getGroup().getGroupID());
             System.out.println("--------------------------------------------");
         }
     }
+
 }
