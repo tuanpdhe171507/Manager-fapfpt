e3eba38907a5de839cc6acf4bece964943f4e35c
tuanpdhe171507 <161819748+tuanpdhe171507@users.noreply.github.com>
2024-03-01 16:58:22 +0700

    tạo model login
    tạo model login

---
+++ MyProjectPRJ301/src/java/model/Login/Campus.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.Login;
+
+/**
+ *
+ * @author G5 5590
+ */
+public class Campus {
+
+    private int id;
+    private String name;
+
+    public Campus() {
+    }
+
+    public Campus(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ MyProjectPRJ301/src/java/model/Login/Login.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.Login;
+
+/**
+ *
+ * @author G5 5590
+ */
+public class Login {
+    private String SelectCampus;
+
+    public Login() {
+    }
+
+    public Login(String SelectCampus) {
+        this.SelectCampus = SelectCampus;
+    }
+
+    public String getSelectCampus() {
+        return SelectCampus;
+    }
+
+    public void setSelectCampus(String SelectCampus) {
+        this.SelectCampus = SelectCampus;
+    }
+    
+    
+}
