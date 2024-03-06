06dab884177bd34543dcaf6d0381a809dafe1f6c
tuanpdhe171507 <161819748+tuanpdhe171507@users.noreply.github.com>
2024-03-07 02:50:41 +0700

    tạo class Role

---
+++ MyProjectPRJ301/src/java/model/Role.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sonnt
+ */
+public class Role {
+    private int id;
+    private String name;
+    private ArrayList<Account> accounts = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(ArrayList<Account> accounts) {
+        this.accounts = accounts;
+    }
+    
+}
