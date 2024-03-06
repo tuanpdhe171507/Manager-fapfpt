e56dbd5b05e7fea0bc544d889f411230117f2e31
tuanpdhe171507 <161819748+tuanpdhe171507@users.noreply.github.com>
2024-03-07 02:50:28 +0700

    Update Attendence.java
    sửa thuộc tính trong class Attendeces

--- MyProjectPRJ301/src/java/model/Attendence.java
+++ MyProjectPRJ301/src/java/model/Attendence.java
@@ -4,6 +4,7 @@
  */
 package model;
 import java.util.Date;
+import java.sql.*;
 /**
  *
  * @author G5 5590
@@ -15,17 +16,33 @@ public class Attendence {
     private String comment;
     private Session session;
     private Student student;
+    private java.sql.Date dateSQL;
+    private Subject subject;
+    private TimeSlot timeslot;
+    private Room room;
 
     public Attendence() {
     }
 
-    public Attendence(int attendenceID, Boolean isPresent, Date datetime, String comment, Session session, Student student) {
+    public Attendence(int attendenceID, Boolean isPresent, Date datetime, String comment, Session session, Student student, java.sql.Date dateSQL, Subject subject, TimeSlot timeslot,Room room) {
         this.attendenceID = attendenceID;
         this.isPresent = isPresent;
         this.datetime = datetime;
         this.comment = comment;
         this.session = session;
         this.student = student;
+        this.dateSQL = dateSQL;
+        this.subject = subject;
+        this.timeslot = timeslot;
+        this.room=room;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
     }
 
     public int getAttendenceID() {
@@ -76,12 +93,37 @@ public class Attendence {
         this.student = student;
     }
 
-    @Override
-    public String toString() {
-        return "Attendence{" + "attendenceID=" + attendenceID + ", isPresent=" + isPresent + ", datetime=" + datetime + ", comment=" + comment + ", session=" + session + ", student=" + student + '}';
+    public java.sql.Date getDateSQL() {
+        return dateSQL;
     }
 
-  
+    public void setDateSQL(java.sql.Date dateSQL) {
+        this.dateSQL = dateSQL;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public TimeSlot getTimeslot() {
+        return timeslot;
+    }
+
+    public void setTimeslot(TimeSlot timeslot) {
+        this.timeslot = timeslot;
+    }
+
+    @Override
+    public String toString() {
+        return "Attendence{" + "attendenceID=" + attendenceID + ", isPresent=" + isPresent + ", datetime=" + datetime + ", comment=" + comment + ", session=" + session + ", student=" + student + ", dateSQL=" + dateSQL + ", subject=" + subject + ", timeslot=" + timeslot + ", room=" + room + '}';
+    }
+
+
+    
     
     
 }
